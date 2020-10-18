5ceb2860f2ea350a2aee7d35a943b5742cf0c8e7
Bloodielie <riopro2812@gmail.com>
2020-10-18 17:04:33 +0300

    refactoring __init__

---
+++ pyject/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.0"
+
+from pyject.container import Container
+from pyject.models import Scope
+from pyject.exception import DependencyNotFound
+from pyject.base import BaseContainer
